4ddc11694cbe4b22860a9f2195509a363b579911
Ayato <ayato3923vrc@yahoo.co.jp>
2024-04-23 18:03:02 +0900

    Add files via upload

--- MainBot.py
+++ MainBot.py
@@ -10,12 +10,12 @@ import mysql.connector
 from mysql.connector import Error
 
 #TOKENを設定
-TOKEN = 'TOKENを指定する。'
+TOKEN = 'トークンを設定してください。'
 #チャンネルIDを設定
-CHANNEL_ID = 'チャンネルIDを書く'    #あそびばチャンネル
-CHANNEL_ID2 = 'チャンネルIDを書く'   #実験用チャンネル
+CHANNEL_ID = 1234567890987654321    #あそびばチャンネル
+CHANNEL_ID2 = 1234567890987654321   #実験用チャンネル
 #鯖管理人のユーザーIDを設定
-user_id = 'ユーザーIDを書く'
+user_id = '23456789012345678'
 
 # 初期化・インスタンス作成
 intents = discord.Intents.all()
@@ -28,10 +28,10 @@ client = discord.Client(intents = intents)
 tree = app_commands.CommandTree(client)
 
 # MySQL接続情報
-MYSQL_HOST = 'HOSTを指定する。'
-MYSQL_USER = 'ユーザー名を指定する。'
-MYSQL_PASSWORD = 'パスワードを指定する。'
-MYSQL_DATABASE = 'データベース名を指定する。'
+MYSQL_HOST = 'Host名を設定してください。'
+MYSQL_USER = 'ユーザー名を設定してください。'
+MYSQL_PASSWORD = 'パスワードを設定してください。'
+MYSQL_DATABASE = ' データベース名を設定してください。'
 # MySQL接続の作成
 mysql_config = {
     'host': MYSQL_HOST,
@@ -55,22 +55,36 @@ def connect_mysql():
 
 #MySQLインサート関数
 def insert_data():
+    global avatar_id
     global avatar_name
     global avatar_url
     global mysql_flag
     try:
         connection = connect_mysql()
         if connection:
+            connection.start_transaction()  #トランザクション開始
             cursor = connection.cursor()
-            insert_query = "INSERT INTO テーブル名 (Col1, Col2) VALUES (%s, %s)"
-            data = (avatar_name, avatar_url)
+            insert_query = "INSERT INTO image_tbl(image_url) VALUES (%s)"
+            data = (avatar_url,)
             cursor.execute(insert_query, data)
+            last_insert_id = cursor.lastrowid
+            print("Last insert ID:", last_insert_id)
+            # 中間テーブルへの挿入
+            insert_query = "INSERT INTO avatar_image_tbl(avatar_id,image_id) VALUES (%s, %s)"
+            data = (avatar_id, last_insert_id)
+            cursor.execute(insert_query, data)
+            # コミット
             connection.commit()
             print("Record inserted successfully")
             mysql_flag = "画像を登録しました。"
     except Error as e:
         print("Error while connecting to MySQL", e)
         mysql_flag = "MySQLに接続できませんでした。"
+        if connection:
+            # ロールバック
+            connection.rollback()
+            print("Transaction rolled back")
+            mysql_flag = "MySQLでロールバックが発生しました。"
     finally:
         if connection:
             cursor.close()
@@ -79,8 +93,24 @@ def insert_data():
 
 #MySQLセレクト関数
 def select_deta():
-    print('今は関数を作成中です。')
-##########################################################################################################
+    global avatar_list
+    try:
+        connection = connect_mysql()
+        if connection:
+            cursor = connection.cursor()
+            select_query = "SELECT * FROM avatar_tbl;"
+            cursor.execute(select_query) #実行
+            avatar_list = [row[1] for row in cursor.fetchall()]
+            print(avatar_list)
+            return avatar_list
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+###########################################################################################################
 # メッセージ関係
 @client.event
 async def on_message(message):
@@ -132,6 +162,7 @@ async def on_message(message):
             await message.channel.send(f'{yuu_result}')
     ##########################################################################################################
     # 以下は実験用チャンネルでのみ反応する（BOT管理権限がある人のみ利用）
+    global avatar_id
     global avatar_name
     global avatar_url
     global mysql_flag
@@ -141,32 +172,42 @@ async def on_message(message):
                 if 'スピカちゃん' in message.content:
                     string = 'スピカ'
                     avatar_name = 'スピカ'
+                    avatar_id = 1
                 elif 'ミラちゃん' in message.content:
                     string = 'ミラ'
+                    avatar_id = 2
                 elif 'ユウちゃん' in message.content:
                     string = 'ユウ'
                     avatar_name = 'ユウ'
+                    avatar_id = 3
                 elif 'リルちゃん' in message.content:
                     string = 'リル'
                     avatar_name = 'リル'
+                    avatar_id = 4
                 elif 'ミルちゃん' in message.content:
                     string = 'ミル'
                     avatar_name = 'ミル'
+                    avatar_id = 5
                 elif 'ティナちゃん' in message.content:
                     string = 'ティナ'
                     avatar_name = 'ティナ'
+                    avatar_id = 6
                 elif 'レイちゃん' in message.content:
                     string = 'レイ'
                     avatar_name = 'レイ'
+                    avatar_id = 7
                 elif 'リリカちゃん' in message.content:
                     string = 'リリカ'
                     avatar_name = 'リリカ'
+                    avatar_id = 8
                 elif 'になちゃん' in message.content:
                     string = 'にな'
                     avatar_name = 'にな'
+                    avatar_id = 9
                 elif 'ちゃやねこ' in message.content:
                     string = 'ちゃやねこ'
                     avatar_name = 'ちゃやねこ'
+                    avatar_id = 0
                 else:
                     string = '名前不明'
                 ##########################################################################################################
@@ -186,6 +227,14 @@ async def on_message(message):
                 #    print('画像が検出されました')
                 #    print(attachment.url)
     ##########################################################################################################
+    global avatar_list
+    if "登録可能アバター"  in message.content:
+        select_deta()
+        # MySQLから取得した要素を一つずつ出して格納する（リスト内包表記）最後に「ちゃん」を付ける
+        result = 'ちゃん、'.join([item for item in avatar_list])
+        result = result + 'ちゃん'
+        await message.channel.send('登録可能アバターは以下の通りです')
+        await message.channel.send(f'{result}')
 
 #スラッシュコマンドの定義
 @tree.command(name="hello",description="挨拶するコマンドです。")
